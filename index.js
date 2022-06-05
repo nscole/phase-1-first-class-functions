27f79e009ba9b4d3334a5ef1b5383071dded0ac6
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 19:04:15 +1000

    Passing 2nd test

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ function receivesAFunction(callbackFunction) {
     return callbackFunction();
 }
 
+function returnsANamedFunction(fn){
+    return function fn(){
+        return "Hello function my name is"
+    };
+}
+
 /*
-describe('returnsANamedFunction()', () => {
+ describe('returnsAnAnonymousFunction()', () => {
     var fn
 
     before(() => {
-      fn = returnsANamedFunction()
+      fn = returnsAnAnonymousFunction()
     })
 
     it('returns a function', () => {
       expect(fn).toBeA('function')
     })
 
-    it('returns a named function', () => {
-      expect(fn.name).toNotEqual('')
+    it('returns an anonymous function', () => {
+      expect(fn.name).toEqual('')
     })
+  })
 */
